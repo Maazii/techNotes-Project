bc5a0421eb5d54ab93029c9287d7b4799cf9297e
Maazii <maaz_rehman39@hotmail.com>
2023-08-05 13:34:02 +0500

    Polishing

--- frontend/src/features/users/usersApiSlice.js
+++ frontend/src/features/users/usersApiSlice.js
@@ -8,10 +8,12 @@ const initialState = usersAdapter.getInitialState();
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
-      query: () => "/users",
-      validateStatus: (response, result) => {
-        return response.status === 200 && !result.isError;
-      },
+      query: () => ({
+        url: "/users",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
       transformResponse: (responseData) => {
         const loadedUsers = responseData.map((user) => {
           user.id = user._id;
@@ -66,16 +68,21 @@ export const {
   useDeleteUserMutation,
 } = usersApiSlice;
 
+// returns the query result object
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
 
-const selectUserData = createSelector(
+// creates memoized selector
+const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult.data
+  (usersResult) => usersResult.data // normalized state object with ids & entities
 );
 
+//getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
   selectIds: selectUserIds,
   // Pass in a selector that returns the users slice of state
-} = usersAdapter.getSelectors((state) => selectUserData(state) ?? initialState);
+} = usersAdapter.getSelectors(
+  (state) => selectUsersData(state) ?? initialState
+);
